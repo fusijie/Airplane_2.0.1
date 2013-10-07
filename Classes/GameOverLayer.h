f6c383c14b3cf60d6b4002b58eff784b3046a39c
Jacky <fusijie@vip.qq.com>
2013-10-07 10:16:31 +0800

    Init Git && Add Proj

---
+++ Classes/GameOverLayer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "cocos2d.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+
+class GameOverLayer :
+	public CCLayer
+{
+public:
+	GameOverLayer(void);
+
+	~GameOverLayer(void);
+
+	virtual bool init();
+
+	static GameOverLayer* create(int passScore);
+
+	void menuBackCallback(CCObject* pSender);
+
+	virtual void keyBackClicked();//Android·µ»Ø¼ü
+
+	void beginChangeHighestScore(CCNode* pNode);
+
+public:
+	
+	int score;
+
+	static int highestHistoryScore;
+
+	CCLabelBMFont* highestScore;
+};
